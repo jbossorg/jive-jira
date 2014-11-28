78b95b5eb9b21893eadcc3c4055bf5a14c09eea7
Libor Krzyzanek <libor@krzyzanek.com>
2014-11-28 14:07:28 +0100

    Upgraded to Jive7. fixes #5

--- src/test/java/org/jboss/community/sbs/plugin/jira/RemoteJiraManagerTest.java
+++ src/test/java/org/jboss/community/sbs/plugin/jira/RemoteJiraManagerTest.java
@@ -17,22 +17,22 @@ public class RemoteJiraManagerTest {
 		assertEquals("http://seamframework.org/Community/AnotherInterceptorProblem",
 				manager.normalizeJiraForumReference("http://seamframework.org/Community/AnotherInterceptorProblem"));
 
-		assertEquals("https://community.jboss.org/message/577806",
-				manager.normalizeJiraForumReference("https://community.jboss.org/message/577806"));
+		assertEquals("https://developer.jboss.org/message/577806",
+				manager.normalizeJiraForumReference("https://developer.jboss.org/message/577806"));
 
 		assertEquals(
 				"http://netty-forums-and-mailing-lists.685743.n2.nabble.com/Netty-3-2-3-Final-released-tp5669985.html",
 				manager
 						.normalizeJiraForumReference("http://netty-forums-and-mailing-lists.685743.n2.nabble.com/Netty-3-2-3-Final-released-tp5669985.html"));
 
-		assertEquals("https://community.jboss.org/message/550657#550657",
-				manager.normalizeJiraForumReference("https://community.jboss.org/message/550657#550657"));
+		assertEquals("https://developer.jboss.org/message/550657#550657",
+				manager.normalizeJiraForumReference("https://developer.jboss.org/message/550657#550657"));
 /* No more supported URL from nukes forums
 		assertEquals("https://community.jboss.org/thread/39512?tstart=0",
 				manager.normalizeJiraForumReference("http://www.jboss.org/index.html?module=bb&op=viewtopic&t=152864"));
 */
 
-		assertEquals("https://community.jboss.org/wiki/SecureTheJmxConsole",
+		assertEquals("https://developer.jboss.org/wiki/SecureTheJmxConsole",
 				manager.normalizeJiraForumReference("http://www.jboss.org/community/docs/DOC-12190"));
 
 	}
