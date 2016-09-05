432790e97c34eb847203554bd46e7b931c46e669
Libor Krzyzanek <libor@krzyzanek.com>
2016-09-05 10:50:18 +0200

    added few testing cases

--- src/test/java/org/jboss/issues/rpc/soap/jirasoapservice_v2/JiraSoapServiceTest.java
+++ src/test/java/org/jboss/issues/rpc/soap/jirasoapservice_v2/JiraSoapServiceTest.java
@@ -7,10 +7,8 @@ package org.jboss.issues.rpc.soap.jirasoapservice_v2;
 
 import javax.xml.rpc.ServiceException;
 
-import com.atlassian.jira.rpc.soap.beans.RemoteGroup;
+import com.atlassian.jira.rpc.soap.beans.RemoteIssue;
 import com.atlassian.jira.rpc.soap.beans.RemoteUser;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 
 /**
  * Simple test class for debugging remote jira soap api
@@ -23,7 +21,7 @@ public class JiraSoapServiceTest {
 
 	private static JiraSoapService getJiraSoapService() throws ServiceException {
 		if (jiraSoapService == null) {
-			String portAddress = "https://issues-stg.jboss.org/rpc/soap/jirasoapservice-v2";
+			String portAddress = "https://issues.stage.jboss.org/rpc/soap/jirasoapservice-v2";
 
 			JiraSoapServiceServiceLocator jiraSoapServiceServiceLocator = new JiraSoapServiceServiceLocator();
 			jiraSoapServiceServiceLocator.setJirasoapserviceV2EndpointAddress(portAddress);
@@ -37,8 +35,13 @@ public class JiraSoapServiceTest {
 
 		RemoteUser user = getJiraSoapService().getUser(jiraToken, "lkrzyzanek");
 		System.out.println("User: " + user);
-		RemoteGroup remoteGroup = getJiraSoapService().getGroup(jiraToken, "JBoss Employee");
 
-		getJiraSoapService().removeUserFromGroup(jiraToken, remoteGroup, user);
+		RemoteIssue[] updatedIssues = getJiraSoapService().getIssuesFromJqlSearch(jiraToken, "updated >= \"-" + 1 + "m\"", 100);
+		System.out.println("updated issues: " + updatedIssues);
+
+
+		//		RemoteGroup remoteGroup = getJiraSoapService().getGroup(jiraToken, "JBoss Employee");
+
+//		getJiraSoapService().removeUserFromGroup(jiraToken, remoteGroup, user);
 	}
 }
